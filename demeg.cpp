ff3146b5e03318269716cf562d71dd1b5f152ce5
saifu9421 <saifursaif905@gmail.com>
2024-11-02 05:34:08 +0600

    demag

---
+++ demeg.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+ int main(){
+      while (true)
+      {
+           for(long long  i =  0; i<=9000000000000000000;i++){
+                cout<<i<<" ";
+           }
+      }
+    main();  
+     return 0;
+ }
